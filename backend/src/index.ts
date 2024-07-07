07666bfdab7be0d8674449ad0c68a360aa039c2f
Mudassar Ali <sahil_bwp@yahoo.com>
2024-07-07 08:53:08 +0400

    format the code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,25 @@
+/*  Load deps
+ * ------------------------- */
 import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
+import apiRoutes from './api/v1/routes';
+import config from './config/config';
 
-dotenv.config();
-
+/* Create Express instance
+ * ----------------------- */
 const app = express();
 const prisma = new PrismaClient();
 
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Start server
-const PORT = process.env.PORT || 3000;
+
+/* Routes
+ * ------------------------- */
+app.use('/api/v1', apiRoutes);
+
+/* Start server
+ * ------------------------- */
+const PORT = config.port;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
